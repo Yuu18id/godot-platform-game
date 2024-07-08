665dd092da063a566b72a420353df1c4b4eb44f9
“Yuu18id“ <211112007@students.mikroskil.ac.id>
2024-07-08 14:27:05 +0700

    add certain elements

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
-
+@onready var jump_sound = $JumpSound
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,6 +17,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get input direction
 	var direction = Input.get_axis("move_left", "move_right")
@@ -35,6 +36,7 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
+		
 	
 	# Apply movement
 	if direction:
